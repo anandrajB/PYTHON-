7738d727fcf9df1d921b11d35af95d00308fa1c1
ANAND RAJ B <53043511+anandchinna98@users.noreply.github.com>
2020-10-08 12:15:33 +0530

    Update day 2.py

--- day 2.py
+++ day 2.py
@@ -35,3 +35,12 @@ if year%4==0:
         print("leap year")
 else:
     print("not a leap year")
+    
+#leap year 2
+
+year=int(input("ENTER YEAR : "))
+if year%4==0 and year%100!=0 or year%400==0 :
+    print("leap year")
+else :
+    print("not a leap year")
+
